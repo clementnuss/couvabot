f1411adba00574144fe74163878f8155d52cb67e
Ludovic <ludovic.giezendanner@epfl.ch>
2016-02-17 21:04:54 +0100

    Dernière version code arduino, avec constantes séparées

--- src/servoSpiControl/servoSpiControl.h
+++ src/servoSpiControl/servoSpiControl.h
@@ -1,18 +1,20 @@
-//
-// Created by clement on 17.02.2016.
-//
+#ifndef SERVOSPICONTROL_SERVOSPICONTROL_H
+#define SERVOSPICONTROL_SERVOSPICONTROL_H
+
 
 int angleConvert    (int rotAngle, int rotSpeed);
 void servoPrepare   (int angle);
-void catchIRCeck    (int side);
+void catchIRCheck   (int side);
 void commandDecoder (void);
 void motorDecoder   (void);
 void spiHandler     (void);
 void freePucks      (void);
 void motorRotation  (void);
 void catchMoveR     (void);
-void catchMoveL     (void);
+void catchMoveG     (void);
 
 void catchPuck(void);
 void catchMoveG(void);
 void catchIRCheck(int side);
+
+#endif //SERVOSPICONTROL_SERVOSPICONTROL_H
